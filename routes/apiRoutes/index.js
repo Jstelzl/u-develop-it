198ae0229379b1ef5c2816c2dda2fbe1e6d1eb7b
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-04 13:05:10 -0400

    finsihed

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = require('./candidateRoutes');
-const routes = express.Router();
+const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
